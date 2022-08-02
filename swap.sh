45965aaf4da679c56b5ec2ddd7d6eac04a7ba9e8
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2022-08-02 12:44:08 +0530

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,13 @@
+#Shell program to swap 2 numbers
+echo "Enter the 2 numbers:"
+read a
+read b
+echo "Before:"
+echo "A=$a"
+echo "B=$b"
+temp=$a
+a=$b
+b=$temp
+echo"After:"
+echo "A=$a"
+echo "B=$b"
