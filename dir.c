9afec9d084f4e1ccb1f6a5535f4a7d97993700d3
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2022-08-02 14:32:55 +0530

    Finished directory program

---
+++ dir.c
@@ -0,0 +1,34 @@
+//C program to show contents in a directory
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+struct dirent *dptr;
+int main(int argc,char argv[])
+{
+    char buff[100];
+    DIR *dirp;
+    printf("Enter the directory name:");
+    scanf("%s",buff);
+    if((dirp=opendir(buff))== NULL)
+    {
+        printf("Directory does not exist");
+        exit(1);
+    }
+    while(dptr=readdir(dirp))
+    {
+        printf("%s \n",dptr->d_name);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
